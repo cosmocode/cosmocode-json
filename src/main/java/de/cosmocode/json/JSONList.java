665df570124500e682ddfd5b605e417df3a4b28d
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2009-10-29 21:10:47 +0000

    finished first draft of JSONList/JSONMap (both pass ~950 unit tests), added test cases (using google collections test framework)

--- src/main/java/de/cosmocode/json/JSONList.java
+++ src/main/java/de/cosmocode/json/JSONList.java
@@ -2,11 +2,12 @@ package de.cosmocode.json;
 
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import de.cosmocode.collections.utility.AbstractUtilityList;
+import de.cosmocode.collections.utility.UtilityList;
 import de.cosmocode.patterns.Adapter;
 
 /**
@@ -18,7 +19,7 @@ import de.cosmocode.patterns.Adapter;
  * @author schoenborn@cosmocode.de
  */
 @Adapter(List.class)
-class JSONList extends AbstractUtilityList<Object> implements List<Object> {
+class JSONList extends AbstractUtilityList<Object> implements List<Object>, UtilityList<Object> {
 
     private final JSONArray array;
     
@@ -30,25 +31,73 @@ class JSONList extends AbstractUtilityList<Object> implements List<Object> {
     public JSONList(JSONArray array) {
         if (array == null) throw new NullPointerException("JSONArray must not be null");
         this.array = array;
+        if (contains(null)) throw new NullPointerException("JSONLIst must not contain null values");
     }
 
     @Override
     public Object get(int index) {
+        if (index < 0 || index >= size()) throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+        final Object value = array.opt(index);
+        if (value instanceof JSONObject) {
+            final JSONObject json = JSONObject.class.cast(value);
+            return JSON.asMap(json);
+        } else if (value instanceof JSONArray) {
+            final JSONArray json = JSONArray.class.cast(value);
+            return JSON.asList(json);
+        } else {
+            return value;
+        }
+    }
+    
+    @Override
+    public Object set(int index, Object element) {
+        if (index < 0 || index >= size()) throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+        if (element == null) throw new NullPointerException("Element must not be null");
         try {
-            return array.get(index);
+            final Object oldValue = get(index);
+            array.put(index, element);
+            return oldValue;
         } catch (JSONException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+    
+    @Override
+    public void add(int index, Object element) {
+        if (index < 0 || index > size()) throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+        if (element == null) throw new NullPointerException("Element must not be null");
+        
+        // save current position
+        int i = index;
+        
+        // holds the next element
+        Object next = element;
+
+        // loop until the end
+        while (true) {
+            // is the current index after the last element?
+            if (i == size()) {
+                // just add the next element
+                array.put(next);
+                // stop looping after adding next element to the end
+                break;
+            } else {
+                // we didn't reach the end, so just overwrite
+                // the element at position i, save the overwritten one
+                // and increase the index
+                next = set(i++, next);
+            }
+        }
+    }
+    
+    @Override
+    public Object remove(int index) {
+        throw new UnsupportedOperationException("Unable to delegate to JSONArray");
     }
 
     @Override
     public int size() {
         return array.length();
-    }
-    
-    @Override
-    public String toString() {
-        return "[" + StringUtils.join(this, ", ") + "]";
     }
     
 }
--- src/main/java/de/cosmocode/json/JSONMap.java
+++ src/main/java/de/cosmocode/json/JSONMap.java
@@ -1,21 +1,17 @@
 package de.cosmocode.json;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.AbstractMap;
-import java.util.Collection;
-import java.util.Date;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
+import java.util.AbstractSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import de.cosmocode.collections.utility.AbstractUtilityMap;
+import de.cosmocode.collections.utility.UtilityMap;
 import de.cosmocode.patterns.Adapter;
 
 /**
@@ -27,287 +23,129 @@ import de.cosmocode.patterns.Adapter;
  * @author schoenborn@cosmocode.de
  */
 @Adapter(Map.class)
-class JSONMap extends AbstractUtilityMap<String, Object> implements Map<String, Object> {
+class JSONMap extends AbstractUtilityMap<String, Object> implements Map<String, Object>, UtilityMap<String, Object> {
 
-    private final JSONObject json;
-    private Set<String> keySet;
-    private Collection<Object> values;
-    private Set<Map.Entry<String, Object>> entrySet;
+    private final JSONObject object;
     
-    /**
-     * TODO replace with type safe generic version from google collections
-     */
-    private final Transformer keyTransformer = new Transformer() {
-        
-        @Override
-        public Object transform(Object key) {
-            return key;
-        }
-        
-    };
-
-    /**
-     * TODO replace with type safe generic version from google collections
-     */
-    private final Transformer valueTransformer = new Transformer() {
-        
-        @Override
-        public Object transform(Object key) {
-            return JSONMap.this.get(key);
-        }
-        
-    };
-
-    /**
-     * TODO replace with type safe generic version from google collections
-     */
-    private final Transformer entryTransformer = new Transformer() {
-        
-        @Override
-        public Object transform(Object key) {
-            return new AbstractMap.SimpleEntry<String, Object>(String.class.cast(key), JSONMap.this.get(key));
-        }
-        
-    };
+    private final Set<Map.Entry<String, Object>> entrySet = new EntrySet();
     
     /**
      * Constructs a new {@link JSONMap} using the specified {@link JSONObject}.
      * 
-     * @param json the {@link JSONObject} this map will be backed by
+     * @param object the {@link JSONObject} this map will be backed by
      */
-    public JSONMap(JSONObject json) {
-        if (json == null) throw new IllegalArgumentException("JSONObject can't be null");
-        this.json = json;
-    }
-    
-    /**
-     * TODO prevent caching
-     */
-    private void reset() {
-        keySet = null;
-        values = null;
-        entrySet = null;
+    public JSONMap(JSONObject object) {
+        if (object == null) throw new IllegalArgumentException("JSONObject can't be null");
+        this.object = object;
     }
     
     @Override
     public int size() {
-        return json.length();
+        return object.length();
     }
     
     @Override
-    public boolean isEmpty() {
-        return json.length() == 0;
+    public boolean containsKey(Object k) {
+        final String key = String.class.cast(k);
+        return object.has(key);
     }
     
     @Override
-    public boolean containsKey(Object key) {
-        return key instanceof String && json.has(String.class.cast(key));
-    }
-    
-    @Override
-    public boolean containsValue(Object value) {
-        return values.contains(value);
-    }
-    
-    @Override
-    public Object get(Object key) {
-        return key instanceof String ? json.opt(String.class.cast(key)) : null;
+    public Object get(Object k) {
+        try {
+            final String key = String.class.cast(k);
+            if (containsKey(key)) {
+                return object.get(key);
+            } else {
+                return null;
+            }
+        } catch (JSONException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
     
     @Override
     public Object put(String key, Object value) {
+        if (key == null) throw new NullPointerException("Key must not be null");
+        if (value == null) throw new NullPointerException("Value must not be null");
         try {
-            return json.put(key, value);
+            final Object oldValue = object.opt(key);
+            object.put(key, value);
+            return oldValue;
         } catch (JSONException e) {
             throw new IllegalArgumentException(e);
-        } finally {
-            reset();
         }
     }
     
     @Override
-    public Object remove(Object key) {
-        try {
-            return key instanceof String ? json.remove(String.class.cast(key)) : null;
-        } finally {
-            reset();
-        }
+    public Object remove(Object k) {
+        final String key = String.class.cast(k);
+        return object.remove(key);
     }
-    
-    @Override
-    public void putAll(Map<? extends String, ? extends Object> map) {
-        for (Map.Entry<? extends String, ? extends Object> entry : map.entrySet()) {
-            put(entry.getKey(), entry.getValue());
-        }
-        reset();
-    }
-    
-    @Override
-    public void clear() {
-        for (String key : keySet()) {
-            remove(key);
-        }
-        reset();
-    }
-    
-    @Override
-    public Set<String> keySet() {
-        if (keySet == null) {
-            keySet = new LinkedHashSet<String>();
-            CollectionUtils.collect(json.keys(), keyTransformer, keySet);
-        }
-        return keySet;
-    }
-    
-    @Override
-    public Collection<Object> values() {
-        if (values == null) {
-            values = new LinkedList<Object>();
-            CollectionUtils.collect(json.keys(), valueTransformer, values);
-        }
-        return values;
-    } 
     
     @Override
     public Set<Map.Entry<String, Object>> entrySet() {
-        if (entrySet == null) {
-            entrySet = new LinkedHashSet<Map.Entry<String, Object>>();
-            CollectionUtils.collect(json.keys(), entryTransformer, entrySet);
-        }
         return entrySet;
     }
     
-    @Override
-    public boolean getBoolean(String key) {
-        try {
-            return json.getBoolean(key);
-        } catch (JSONException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-    
-    @Override
-    public boolean getBoolean(String key, boolean defaultValue) {
-        return json.optBoolean(key, defaultValue);
-    }
-    
-    @Override
-    public byte getByte(String key, byte defaultValue) {
-        return containsKey(key) ? Number.class.cast(json.optInt(key)).byteValue() : defaultValue;
-    }
-    
-    @Override
-    public short getShort(String key, short defaultValue) {
-        return containsKey(key) ? Number.class.cast(json.optInt(key)).shortValue() : defaultValue;
-    }
-    
-    @Override
-    public int getInt(String key) throws IllegalArgumentException {
-        try {
-            return json.getInt(key);
-        } catch (JSONException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-    
-    @Override
-    public int getInt(String key, int defaultValue) {
-        return json.optInt(key, defaultValue);
-    }
-    
-    @Override
-    public long getLong(String key) throws IllegalArgumentException {
-        try {
-            return json.getLong(key);
-        } catch (JSONException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-    
-    @Override
-    public long getLong(String key, long defaultValue) {
-        return json.optLong(key, defaultValue);
-    }
-    
-    @Override
-    public float getFloat(String key, float defaultValue) {
-        return containsKey(key) ? Number.class.cast(json.optDouble(key)).floatValue() : defaultValue;
-    }
-    
-    @Override
-    public double getDouble(String key) throws IllegalArgumentException {
-        try {
-            return json.getDouble(key);
-        } catch (JSONException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-    
-    @Override
-    public double getDouble(String key, double defaultValue) {
-        return json.optDouble(key, defaultValue);
-    }
-    
-    @Override
-    public BigDecimal getBigDecimal(String key) throws IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public BigInteger getBigInteger(String key) throws IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public BigInteger getBigInteger(String key, BigInteger defaultValue) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public Date getDate(String key) throws IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public Date getDate(String key, Date defaultValue) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public <T extends Enum<T>> T getEnum(String key, Class<T> enumType) throws IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    /**
+     * Inner class serving the purpose of an {@link Entry} set.
+     * 
+     * @author schoenborn@cosmocode.de
+     */
+    private class EntrySet extends AbstractSet<Map.Entry<String, Object>> {
 
-    @Override
-    public <T extends Enum<T>> T getEnum(String key, Class<T> enumType, T defaultValue) {
-        // TODO Auto-generated method stub
-        return null;
-    };
-    
-    @Override
-    public String getString(String key) throws IllegalArgumentException {
-        try {
-            return json.getString(key);
-        } catch (JSONException e) {
-            throw new IllegalArgumentException(e);
+        /**
+         * Inner class serving the purpose of an {@link Iterator} of the {@link Entry}s of
+         * this {@link Map}.
+         * 
+         * @author schoenborn@cosmocode.de
+         */
+        private class EntrySetIterator implements Iterator<Map.Entry<String, Object>> {
+            
+            private final Iterator<?> iterator = object.keys();
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+            
+            @Override
+            public Map.Entry<String, Object> next() {
+                final String entryKey = iterator.next().toString();
+                final Object value = get(entryKey);
+                final Object entryValue;
+                
+                if (value instanceof JSONObject) {
+                    final JSONObject json = JSONObject.class.cast(value);
+                    entryValue = JSON.asMap(json);
+                } else if (value instanceof JSONArray) {
+                    final JSONArray json = JSONArray.class.cast(value);
+                    entryValue = JSON.asList(json);
+                } else {
+                    entryValue = value;
+                }
+                
+                return new AbstractMap.SimpleEntry<String, Object>(entryKey, entryValue);
+            }
+            
+            @Override
+            public void remove() {
+                iterator.remove();
+            }
+            
         }
-    }
-    
-    @Override
-    public String getString(String key, String defaultValue) {
-        return json.optString(key, defaultValue);
+        
+        @Override
+        public Iterator<Map.Entry<String, Object>> iterator() {
+            return new EntrySetIterator();
+        }
+
+        @Override
+        public int size() {
+            return object.length();
+        }
+        
     }
     
 }
